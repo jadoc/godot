c5f72a9208e015a7669a459a84fea1207b5acafc
Alex Docauer <alex@docauer.net>
2024-08-20 22:24:01 -0700

    Only reset tooltip timer when mouse actually moved
    InputEventMouseMotion isn't guaranteed to fire only on actual mouse
    movement. It's not uncommon for the underlying OS motion event to be
    sent either by the OS itself or another application even though the
    mouse hasn't moved.  Godot will generate such zero-motion
    InputEventMouseMotion events itself for things like cursor shape
    changes.
    
    This is a backport of 327611f534e2646fd512fdacde8a2b0f2d8b7598 to the
    3.x branch.

--- scene/main/viewport.cpp
+++ scene/main/viewport.cpp
@@ -2254,7 +2254,7 @@ void Viewport::_gui_input_event(Ref<InputEvent> p_event) {
 				}
 			}
 
-			if (can_tooltip && !is_tooltip_shown && over->can_process()) {
+			if (can_tooltip && !is_tooltip_shown && over->can_process() && !mm->get_relative().is_zero_approx()) {
 				if (gui.tooltip_timer.is_valid()) {
 					gui.tooltip_timer->release_connections();
 					gui.tooltip_timer = Ref<SceneTreeTimer>();
